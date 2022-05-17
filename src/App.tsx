f89f582923b06035e6f43703c79b42abad160313
DanielRolfs <danielrolfs@icloud.com>
2022-05-17 12:27:00 +0200

    style menu bar

--- src/App.tsx
+++ src/App.tsx
@@ -48,22 +48,44 @@ function useRouteMatch(patterns: readonly string[]) {
   return null;
 }
 
+
+
 function MyTabs() {
   // You need to provide the routes in descendant order.
   // This means that if you have nested routes like:
   // users, users/new, users/edit.
   // Then the order should be ['users/add', 'users/edit', 'users'].
-  const routeMatch = useRouteMatch(['/', '/admin', '/developer-docs', '/about']);
+  const routeMatch = useRouteMatch(['', '/admin', '/developer-docs', '/about']);
   const currentTab = routeMatch?.pattern?.path;
+  console.log(currentTab)
 
   return (
     <Tabs value={currentTab}>
-      <Tab label="Catalog" value="/" to="/" component={Link} />
+      <Tab label="Catalog" value="" to="" component={Link} />
       <Tab label="Admin" value="/admin" to="/admin" component={Link} />
       <Tab label="Developer Docs" value="/developer-docs" to="/developer-docs" component={Link} />
       <Tab label="About" value="/about" to="/about" component={Link} />
     </Tabs>
   );
+}
+
+function MenuBar() {
+  
+  return (
+    <>
+      <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+        <div>
+          Logo
+        </div>
+        <div>
+          <MyTabs />
+        </div>
+        <div>
+          Icon
+        </div>
+      </div>
+    </>
+   );
 }
 
 function CurrentRoute() {
@@ -86,9 +108,9 @@ function App() {
 
       <Router>
         <Box sx={{ width: '100%' }}>
-          <MyTabs />
+          <MenuBar />
           <Routes>
-            <Route path="/" element={<Catalog />} />
+            <Route path="*" element={<Catalog />} />
             <Route path="/admin" element={<Admin />} />
             <Route path="/developer-docs" element={<DeveloperDocs />} />
             <Route path="/about" element={<About />} />
