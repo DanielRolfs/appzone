ccaf1911b34b013bf3bbbf551bf4b5d2ec483252
DanielRolfs <danielrolfs@icloud.com>
2022-05-17 16:55:51 +0200

    adapt to our needs

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -9,7 +9,7 @@ import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 
 interface Column {
-    id: 'name' | 'code' | 'population' | 'size' | 'density';
+    id: 'name' | 'preview' | 'production' | 'appowner' | 'placeholder';
     label: string;
     minWidth?: number;
     align?: 'right';
@@ -18,64 +18,63 @@ interface Column {
 
 const columns: readonly Column[] = [
     { id: 'name', label: 'Name', minWidth: 170 },
-    { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+    { id: 'preview', label: 'Preview', minWidth: 100 },
     {
-        id: 'population',
-        label: 'Population',
+        id: 'production',
+        label: 'Production',
         minWidth: 170,
-        align: 'right',
-        format: (value: number) => value.toLocaleString('en-US'),
     },
     {
-        id: 'size',
-        label: 'Size\u00a0(km\u00b2)',
+        id: 'appowner',
+        label: 'App Owner',
         minWidth: 170,
-        align: 'right',
-        format: (value: number) => value.toLocaleString('en-US'),
     },
     {
-        id: 'density',
-        label: 'Density',
+        id: 'placeholder',
+        label: 'Placeholder',
         minWidth: 170,
-        align: 'right',
-        format: (value: number) => value.toFixed(2),
     },
 ];
 
 interface Data {
     name: string;
-    code: string;
-    population: number;
-    size: number;
-    density: number;
+    preview: string;
+    production: string;
+    appowner: string;
+    placeholder: string;
 }
 
 function createData(
     name: string,
-    code: string,
-    population: number,
-    size: number,
+    preview: string,
+    production: string,
+    appowner: string,
 ): Data {
-    const density = population / size;
-    return { name, code, population, size, density };
+    const placeholder = 'Testdata'
+    return { name, preview, production, appowner, placeholder };
 }
 
 const rows = [
-    createData('India', 'IN', 1324171354, 3287263),
-    createData('China', 'CN', 1403500365, 9596961),
-    createData('Italy', 'IT', 60483973, 301340),
-    createData('United States', 'US', 327167434, 9833520),
-    createData('Canada', 'CA', 37602103, 9984670),
-    createData('Australia', 'AU', 25475400, 7692024),
-    createData('Germany', 'DE', 83019200, 357578),
-    createData('Ireland', 'IE', 4857000, 70273),
-    createData('Mexico', 'MX', 126577691, 1972550),
-    createData('Japan', 'JP', 126317000, 377973),
-    createData('France', 'FR', 67022000, 640679),
-    createData('United Kingdom', 'GB', 67545757, 242495),
-    createData('Russia', 'RU', 146793744, 17098246),
-    createData('Nigeria', 'NG', 200962417, 923768),
-    createData('Brazil', 'BR', 210147125, 8515767),
+    createData('Automated Time Sheets', 'Release-1', 'Release-1', 'test_dev@blu-beyond.com'),
+    createData('Book my Desk', 'Release-3', 'Release-3', 'test_dev@blu-beyond.com'),
+    createData('Testdata', 'Testdata', 'Testdata', 'Testdata'),
+    createData('Testdata', 'Testdata', 'Testdata', 'Testdata'),
+    createData('Testdata', 'Testdata', 'Testdata', 'Testdata'),
+    createData('Testdata', 'Testdata', 'Testdata', 'Testdata'),
+    createData('Testdata', 'Testdata', 'Testdata', 'Testdata'),
+    createData('Testdata', 'Testdata', 'Testdata', 'Testdata'),
+    createData('Testdata', 'Testdata', 'Testdata', 'Testdata'),
+    createData('Testdata', 'Testdata', 'Testdata', 'Testdata'),
+    createData('Testdata', 'Testdata', 'Testdata', 'Testdata'),
+    createData('Testdata', 'Testdata', 'Testdata', 'Testdata'),
+    createData('Testdata', 'Testdata', 'Testdata', 'Testdata'),
+    createData('Testdata', 'Testdata', 'Testdata', 'Testdata'),
+    createData('Testdata', 'Testdata', 'Testdata', 'Testdata'),
+    createData('Testdata', 'Testdata', 'Testdata', 'Testdata'),
+    createData('Testdata', 'Testdata', 'Testdata', 'Testdata'),
+    createData('Testdata', 'Testdata', 'Testdata', 'Testdata'),
+    createData('Testdata', 'Testdata', 'Testdata', 'Testdata'),
+
 ];
 
 export default function Admin() {
@@ -113,7 +112,7 @@ export default function Admin() {
                             .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                             .map((row) => {
                                 return (
-                                    <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                                    <TableRow hover role="checkbox" tabIndex={-1} key={row.preview}>
                                         {columns.map((column) => {
                                             const value = row[column.id];
                                             return (
